ad90837873fed65e93d23070321df67faf4f0b59
dandi-maulana <nugraha.rahmadan1304@gmail.com>
2025-05-23 19:13:37 +0700

    learning text widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,22 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.greenAccent,
           title: Text("My APP"),
         ),
-        body: Center(child: Text('Hello, World!')),
+        body: Center(
+          child: Text(
+            'The code you provided is good enough and works well to create a simple Flutter app. However, there are some fixes and improvements you can make to follow best practices and add some features.',
+            // maxLines: 2,
+            // overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.right,
+            style: TextStyle(
+              fontSize: 20,
+              backgroundColor: Colors.blue,
+              fontFamily: "Boldyguard",
+              decorationStyle: TextDecorationStyle.wavy,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.amberAccent,
+            ),
+          ),
+        ),
       ),
     );
   }
